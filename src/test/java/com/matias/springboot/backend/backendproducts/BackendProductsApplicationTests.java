b2c5dc5497d1a174262a2dfbd181ba381537465b
Matias Navio <naviomatias2@gmail.com>
2024-05-20 16:03:08 -0300

    Initial commit

---
+++ src/test/java/com/matias/springboot/backend/backendproducts/BackendProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.backend.backendproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
